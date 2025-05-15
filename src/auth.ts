0b3c362268f0c90bd3ac989528f329bdd269d322
Ivan Paghubasan <ivan.paghubasan@hoolisoftware.com>
2025-05-15 21:00:32 +0800

    Update prisma configuration

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,13 @@
-import NextAuth from "next-auth"
-import authConfig from "./auth.config"
- 
-import { PrismaClient } from "@prisma/client"
-import { PrismaAdapter } from "@auth/prisma-adapter"
- 
-const prisma = new PrismaClient()
- 
-export const { handlers: {GET, POST}, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  session: { strategy: "jwt" },
-  ...authConfig,
-})
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./lib/prisma";
+
+export const {
+    handlers: { GET, POST },
+    auth,
+} = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    session: { strategy: "jwt" },
+    ...authConfig,
+});
